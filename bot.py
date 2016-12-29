6d3efc9aba274a33f8f5167f2f66dd52b169ef17
Людмила <kornilova203@gmail.com>
2016-12-29 14:19:59 +0300

    config and bot was added for test

--- bot.py
+++ bot.py
@@ -1,22 +1,11 @@
+import config
 import telebot
-import os
-from flask import Flask, request
 
-bot = telebot.TeleBot('321375343:AAET-7j3Pz0WJtTSrpLY60KDl0NTfChxGQI')
+bot = telebot.TeleBot(config.token)
 
-server = Flask(__name__)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
-@server.route("/bot", methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "!", 200
-
-@server.route("/")
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
