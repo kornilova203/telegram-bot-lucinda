b2977c87a83bea625ece63d0484c865746fcf9cc
Lyudmila Kornilova <kornilova203@gmail.com>
2017-02-01 16:33:31 +0300

    changed name of examples shelve

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 token = '328988293:AAEM4PebXc1RckiHc55aQZmNiGGdeGPi5XE'
 dictName = "dictShelve"
-examplesDictName = "examplesShelve.db"
+examplesDictName = "examplesShelve"
 headers = {
     "X-Mashape-Key": "dtW1yc4LIfmshH9TUrGvuKSm69cVp1TtKIejsntpNcrTe3XXmP",
     "Accept": "application/json"
 }
-
